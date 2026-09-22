9238d4d014c6d37222bf5c4cc3c59ad8a67a406f
laurasantanamachado-source <laurasantanamachado@gmail.com>
2026-09-22 00:35:54 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function mostrarDica() {
+    document.getElementById("mensagemDica").innerText =
+        "💡 Use as habilidades dos quatro personagens para superar os desafios!";
+}
+
+function jogar() {
+    document.getElementById("statusJogo").innerText =
+        "🎮 O jogo ainda está sendo desenvolvido!";
+}
+
+function baixar() {
+    document.getElementById("statusJogo").innerText =
+        "⬇️ O download estará disponível quando o jogo ficar pronto!";
+}
